bb3667214a67a1db7e7c8a478baee1ebb61c910a
shivji <jindalshivam65@gmail.com>
2015-12-22 18:48:47 +0530

    initial commit

---
+++ host.py
@@ -0,0 +1,32 @@
+import os
+import jinja2
+import webapp2
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+from xml.dom import minidom
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self,*a,**kw):
+		self.response.out.write(*a,**kw)
+
+	def render_str(self,template,**params):
+		t=jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self,template,**kw):
+		self.write(self.render_str(template,**kw))
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+
+class MainPage(Handler):
+
+	def get(self):
+		self.render('index.html');
+app = webapp2.WSGIApplication([('/',MainPage),],debug=True)
